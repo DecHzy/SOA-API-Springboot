151c359872485943526c1e119e2ddd40e365c1dd
dec <1503409765@qq.com>
2022-12-19 10:32:41 +0800

    SpringBoot 初始化

---
+++ src/main/java/org/csu/soa/SoaApiApplication.java
@@ -0,0 +1,13 @@
+package org.csu.soa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaApiApplication.class, args);
+    }
+
+}
